7e744c78a90607252789b33912aedbe55d3e087a
Clyde Geyer <geyer.clyde@gmail.com>
2024-01-31 18:05:35 -0500

    updates

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import feedparser
+from discord_webhook import DiscordWebhook
 
 xml_url = 'https://www.monroecounty.gov/incidents911.rss'
+
+webhook_url = 'https://discordapp.com/api/webhooks/1202388231430357032/88n4s0oek0rf1XrCLu9BJn7lnGJfNdospM7Gm_mubuUdf7kRI4GjS5gdg2dzkdF3QjMd'
 
 feed = feedparser.parse(xml_url)
 
@@ -10,8 +13,12 @@ def parse():
         print(entry)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    parse()
+def test_webhook():
+    message = 'testing webhook!'
+    webhook = DiscordWebhook(url=webhook_url, content=message)
+    response = webhook.execute()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == '__main__':
+    test_webhook()
+

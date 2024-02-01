fc1793057bfe8defc2bdb3fd071cf44cf2f75987
arctic <geyer.clyde@gmail.com>
2024-01-31 19:22:55 -0500

    started adding ntfy integration, working locally

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import feedparser
+import requests
 from discord_webhook import DiscordWebhook
 
 xml_url = 'https://www.monroecounty.gov/incidents911.rss'
@@ -9,16 +10,26 @@ feed = feedparser.parse(xml_url)
 
 
 def parse():
-    for entry in feed['entries']:
-        print(entry)
+    # for entry in feed['entries']:
+    #     print(entry)
+
+    print(feed.entries[0]['title'])
 
 
 def test_webhook():
     message = 'testing webhook!'
     webhook = DiscordWebhook(url=webhook_url, content=message)
-    response = webhook.execute()
+    webhook.execute()
+
+
+def test_ntfy():
+    requests.post("http://192.168.0.92/test", data="test message!".encode(encoding='utf-8'))
+
+
+def sending_data_test():
+    message = feed.entries[0]['title']
+    requests.post("http://192.168.0.92/test", data=message.encode(encoding='utf-8'))
 
 
 if __name__ == '__main__':
-    test_webhook()
-
+    sending_data_test()
